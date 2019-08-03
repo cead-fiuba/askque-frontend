f282b02d7eb72b53478d77fea713fb34e97ed0e2
crisnicogonzalez <cristian.gonzalez@despegar.com>
2019-08-02 21:05:51 -0300

    delete unused imports

--- src/components/MyAskques.js
+++ src/components/MyAskques.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import AppBar from "./AppBar"
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import EditIcon from '@material-ui/icons/Edit';
-import ShareIcon from '@material-ui/icons/Share'
 import AskqueResume from "./AskqueResume"
 
 const useStyles = makeStyles(theme => ({
