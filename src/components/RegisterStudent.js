4a5a73798dd6594463c68b017de52fd85ccb6507
crisnicogonzalez <cristian.gonzalez@despegar.com>
2019-08-09 01:48:17 -0300

    add create account button

--- src/components/RegisterStudent.js
+++ src/components/RegisterStudent.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles(theme => ({
+    createAcccountButton: {
+        marginTop: theme.spacing(2),
+    }
+}));
 
 export default function RegisterStudent() {
 
-    //const classes = useStyles();
+    const classes = useStyles();
 
     return <div>
         <Container component="main" >
@@ -51,6 +59,10 @@ export default function RegisterStudent() {
                 fullWidth
                 variant="outlined"
             />
+
+            <Button variant="contained" color="secondary" fullWidth className={classes.createAcccountButton}>
+                Crear cuenta AskQue
+            </Button>
         </Container>
     </div>
 }
