16e9d4b2961419df53c80b0cdf9bc3c651f2bc4a
crisnicogonzalez <cristian.gonzalez@despegar.com>
2019-08-26 20:20:07 -0300

    add logs

--- src/Server.js
+++ src/Server.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const SERVER_PATH = process.env.NODE_ENV == "PROD" ? "https://askque-back.herokuapp.com/" : "http://localhost:3000/";
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 const token = localStorage.getItem('token')
 const server = axios.create({
     baseURL: SERVER_PATH,
