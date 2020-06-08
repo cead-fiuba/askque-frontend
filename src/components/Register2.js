5d5a624852ef0ba1298f57964c4d67ac56ec8776
crisnicogonzalez <cristian3629@gmail.com>
2020-06-08 19:55:55 -0300

    wip

--- src/components/Register2.js
+++ src/components/Register2.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -15,6 +14,19 @@ import { MyGoogleButton } from "./GoogleButton";
 import Button from "@material-ui/core/Button";
 import InfoIcon from "@material-ui/icons/InfoOutlined";
 import DoneAllIcon from "@material-ui/icons/DoneAllRounded";
+import Divider from "@material-ui/core/Divider";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { isEmpty } from "../util/StringUtil";
+import { createStudent } from "../service/StudentService";
+import { createTeacher } from "../service/TeacherService";
+import { AppContextConsumer } from "../context/context";
+import { withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,13 +45,17 @@ const useStyles = makeStyles((theme) => ({
   googleButton: {
     width: "100% !important",
   },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
 }));
 
 const emails = { 1: "@fi.uba.ar", 0: "@gmail.com" };
 const asignatures = ["Fisica I", "Fisica II"];
 const ocupations = ["Profesor", "Ayudante", "JTP"];
 
-export default function Register() {
+function Register(props) {
   const [userData, setUserData] = React.useState({
     name: null,
     lastname: null,
@@ -51,6 +67,8 @@ export default function Register() {
 
   const [isComplete, setIsComplete] = React.useState();
   const [emailWasValidated, setEmailWasValidated] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [showDialog, setShowDialog] = React.useState(false);
 
   const handleChange = (name) => (event) => {
     setUserData({ ...userData, [name]: event.target.value });
@@ -61,13 +79,11 @@ export default function Register() {
   }, [userData, emailWasValidated]);
 
   const checkIsComplete = () => {
-    const nameIsOk = userData.name !== "" && userData.name !== null;
-    const lastNameIsOk = userData.lastname !== "" && userData.lastname !== null;
+    const nameIsOk = !isEmpty(userData.name);
+    const lastNameIsOk = !isEmpty(userData.lastname);
     const rolIsOk = userData.rol !== "";
-    const emailUserNameIsOk =
-      userData.emailUserName !== null && userData.emailUserName !== "";
-    const emailDomineIsOk =
-      userData.emailDomine !== null && userData.emailDomine !== "";
+    const emailUserNameIsOk = !isEmpty(userData.emailUserName);
+    const emailDomineIsOk = !isEmpty(userData.emailDomine);
 
     const basicValidationIsOk =
       nameIsOk &&
@@ -76,37 +92,22 @@ export default function Register() {
       emailUserNameIsOk &&
       emailDomineIsOk &&
       emailWasValidated;
-    console.log("nameIsOK", nameIsOk);
-    console.log("lastNameIsOk", lastNameIsOk);
-    console.log("rolIsOk", rolIsOk);
-    console.log("emailUserNameIsOk", emailUserNameIsOk);
-    console.log("emailDomineIsOk", emailDomineIsOk);
-    console.log("emailWasValidated", emailWasValidated);
 
     if (basicValidationIsOk) {
       if (userData.rol === 0) {
-        console.log("estudiante");
-        const padronIsOk =
-          userData.padron !== "" &&
-          userData.padron !== null &&
-          userData.padron !== undefined;
-        console.log("padron", userData.padron);
+        const padronIsOk = !isEmpty(userData.padron);
         setIsComplete(padronIsOk);
       } else if (userData.rol === 1) {
-        const legajoIsOk =
-          userData.legajo !== "" &&
-          userData.legajo !== undefined &&
-          userData.legajo !== null;
-        const asignatureIsOk =
-          userData.asignature !== null &&
-          userData.asignature !== "" &&
-          userData.asignature !== undefined;
+        const legajoIsOk = !isEmpty(userData.legajo);
+        const asignatureIsOk = !isEmpty(userData.asignature);
         setIsComplete(legajoIsOk && asignatureIsOk);
       }
     }
   };
 
-  const classes = useStyles();
+  const goTo = (value) => {
+    props.history.push(value);
+  };
 
   const loginCallback = (email) => {
     const currentEmail = userData.emailUserName + emails[userData.emailDomine];
@@ -115,6 +116,46 @@ export default function Register() {
       setEmailWasValidated(true);
     }
   };
+
+  const save = () => {
+    setLoading(true);
+    const email = userData.emailUserName + emails[userData.emailDomine];
+    if (userData.rol === 0) {
+      const request = {
+        name: userData.name,
+        lastname: userData.lastname,
+        padron: userData.padron,
+        email: email,
+      };
+      createStudent(request).then((res) => {
+        props.context.setToken(res.data.token);
+        props.context.setEmail(email);
+        setTimeout(() => {
+          setLoading(false);
+          setShowDialog(true);
+        }, 2000);
+      });
+    } else {
+      const request = {
+        legajo: userData.legajo,
+        name: userData.name,
+        lastname: userData.lastname,
+        asignature: asignatures[userData.asignature],
+        email: email,
+        ocupation: userData.ocupation,
+      };
+      createTeacher(request).then((res) => {
+        props.context.setToken(res.data.token);
+        props.context.setEmail(email);
+        setTimeout(() => {
+          setLoading(false);
+          setShowDialog(true);
+        }, 2000);
+      });
+    }
+  };
+
+  const classes = useStyles();
 
   return (
     <div>
@@ -165,6 +206,8 @@ export default function Register() {
             </Grid>
             <Grid item xs={7} />
           </Grid>
+
+          <Divider light style={{ marginTop: "2%" }} />
 
           {userData.rol === 1 && (
             <>
@@ -394,9 +437,7 @@ export default function Register() {
               <Button
                 variant="contained"
                 className={classes.cancelButton}
-                onClick={() => {
-                  console.log("cancel");
-                }}
+                onClick={save}
                 color="primary"
               >
                 Crear cuenta
@@ -405,6 +446,50 @@ export default function Register() {
           )}
         </Paper>
       </Container>
+
+      <Backdrop className={classes.backdrop} open={loading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Dialog
+        open={showDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Creación de cuenta exitosa"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <>
+              Usted acaba de crear una cuenta como{" "}
+              <b>{userData.rol === 0 ? "alumno" : "docente"}</b>. Esto significa
+              que solo puede
+              <>{userData.rol === 0 ? "responder" : "crear"}</> cuestionarios
+            </>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              if (userData.rol === 0) {
+                goTo("/ask-questionary");
+              } else {
+                goTo("/my-questionaries");
+              }
+            }}
+            color="primary"
+            autoFocus
+          >
+            {userData.rol === 0 ? "Response cuestionario" : "Mis cuestionarios"}
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
+
+export default withRouter((props) => (
+  <AppContextConsumer>
+    {(contextData) => <Register {...props} context={contextData} />}
+  </AppContextConsumer>
+));
---
+++ src/util/StringUtil.js
@@ -0,0 +1,3 @@
+export const isEmpty = (string) => {
+  return string === null && string === undefined && string === "";
+};
