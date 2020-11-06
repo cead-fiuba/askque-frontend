6f670a7dc66010d56ef53d7daaf38035e2651311
Santiago Beroch <sberoch@gmail.com>
2020-11-06 19:04:33 -0300

    fix route

--- src/components/Root.js
+++ src/components/Root.js
@@ -19,6 +19,7 @@ export default class Root extends Component {
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/create-questionary" component={CreateQuestionary} />
+          <Route exact path="/ask-questionary" component={AskQuestionaryView} />
           <Route path="/ask-questionary/:hash" component={AskQuestionaryView} />
           <Route path="/my-questionaries" component={MyAskques} />
           <Route path="/my-answers" component={MyAnswersView} />
