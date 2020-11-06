8bfcded484992ff5c8f01909339cea737523059e
sberoch <sberoch@gmail.com>
2020-11-06 18:23:43 -0300

    added https redirects to all requests

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Root from "./components/Root"
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { AppContextProvider } from '../src/context/context'
 import CssBaseline from '@material-ui/core/CssBaseline';
+import HttpsRedirect from 'react-https-redirect';
 
 const theme = createMuiTheme({
   palette: {
@@ -20,12 +21,16 @@ const theme = createMuiTheme({
 
 class App extends Component {
   render() {
-    return <MuiThemeProvider theme={theme}>
-      <AppContextProvider>
-        <CssBaseline />
-        <Root />
-      </AppContextProvider>
-    </MuiThemeProvider>
+    return (
+      <HttpsRedirect>
+        <MuiThemeProvider theme={theme}>
+          <AppContextProvider>
+            <CssBaseline />
+            <Root />
+          </AppContextProvider>
+        </MuiThemeProvider>
+      </HttpsRedirect>
+    );
   }
 }
 
